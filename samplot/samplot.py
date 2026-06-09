19a3c9674437f56175f4f375789c73bb08ec6bd6
anastasia <s2410458018@fhooe.at>
2026-06-09 13:49:11 +0200

    add legend entry for "Missing mate paired-end read"

--- samplot/samplot.py
+++ samplot/samplot.py
@@ -44,6 +44,7 @@ READ_TYPES_USED = {
     "Linked read": False,
     "Split-read": False,
     "Paired-end read": False,
+    "Missing mate paired-end read": False
 }
 
 # pysam.readthedocs.io/en/latest/api.html#pysam.AlignedSegment.cigartuples
@@ -643,7 +644,6 @@ def get_pair_plan(ranges, pair, linked_plan=False):
         max(first.pos.start, ranges[first_hit].start),
     )
 
-    insert_size = 0
     if not mate_missing:
         insert_size = get_pair_insert_size(ranges, pair)
 
@@ -653,6 +653,8 @@ def get_pair_plan(ranges, pair, linked_plan=False):
             min(second.pos.end, ranges[second_hit].end),
         )
     else:
+        insert_size = 2000
+
         end = genome_interval(
             first.pos.chrm,
             min(first.pos.end, ranges[first_hit].end),
@@ -665,7 +667,8 @@ def get_pair_plan(ranges, pair, linked_plan=False):
                  "INSERTSIZE": insert_size}
 
     if mate_missing:
-        step.info["MATE_UNMAPPED_STRAND"] = first.strand
+        step.info["MATE_IS_UNMAPPED"] = first.strand
+
 
     return insert_size, step
 # }}}
@@ -739,18 +742,20 @@ def plot_pair_plan(ranges, step, ax, marker_size, jitter_bounds):
     color = COLORS[event_type]
 
     y_jitt = jitter(y, 0.9)
+    read_len = step.start_pos.end - step.start_pos.start
 
     x_start = p[0]
     x_end = p[1]
 
-    if "MATE_UNMAPPED_STRAND" in step.info:
-        if step.info["MATE_UNMAPPED_STRAND"]:
+    if "MATE_IS_UNMAPPED" in step.info:
+        READ_TYPES_USED["Missing mate paired-end read"] = True
+        if step.info["MATE_IS_UNMAPPED"] == True:
             #if mate is on forward strand
             ax.plot(
                 [x_start, x_end],
                 [y_jitt, y_jitt],
                 "-",
-                color="deeppink",
+                color="magenta",
                 alpha=0.5,
                 # 0.7 instead of 0.5 for better visibility
                 lw=1.0,
@@ -764,7 +769,7 @@ def plot_pair_plan(ranges, step, ax, marker_size, jitter_bounds):
                 [x_start, x_end],
                 [y_jitt, y_jitt],
                 "-",
-                color="deeppink",
+                color="magenta",
                 alpha=0.5,
                 # 0.7 instead of 0.5 for better visibility
                 lw=1.0,
@@ -773,20 +778,24 @@ def plot_pair_plan(ranges, step, ax, marker_size, jitter_bounds):
                 markevery=[1],
                 zorder=10,
             )
-    else:
-        ax.plot(
-            p,
-            [y, y],
-            "-",
-            color=color,
-            alpha=0.25,
-            lw=0.5,
-            marker="s",
-            markersize=marker_size,
-            zorder=10,
-        )
+
+        return True
+
+    # plot the individual pair
+    ax.plot(
+        p,
+        [y, y],
+        "-",
+        color=color,
+        alpha=0.25,
+        lw=0.5,
+        marker="s",
+        markersize=marker_size,
+        zorder=10,
+    )
 
     return True
+
 
 # }}}
 
@@ -3085,6 +3094,24 @@ def plot_legend(fig, legend_fontsize, marker_size):
                 lw=1,
             )
         ]
+
+    if READ_TYPES_USED["Missing mate paired-end read"]:
+        marker_labels.append("Missing mate paired-end read")
+        legend_elements += [
+            plt.Line2D(
+                [0, 1],
+                [0, 0],
+                markerfacecolor="None",
+                markeredgecolor="magenta",
+                color="magenta",
+                marker="|",
+                markersize=marker_size*1.5,
+                linestyle="-",
+                lw=1,
+                markevery=[0]
+            )
+        ]
+
 
     fig.legend(
         legend_elements, marker_labels, loc=1, fontsize=legend_fontsize, frameon=False
